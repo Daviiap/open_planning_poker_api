c0ac27a31d6c041284466a757d924e9c932597bd
Daviiap <davii.sapiens@gmail.com>
2024-08-27 09:12:25 -0300

    fixing env and removing db file from git

--- src/env.ts
+++ src/env.ts
@@ -3,7 +3,7 @@ import * as dotenv from "dotenv";
 dotenv.config();
 
 export default {
-    API_PORT: parseInt(process.env.API_PORT as string, 10),
+    API_PORT: parseInt(process.env.PORT as string, 10),
     BCRYPT_ROUNDS: parseInt(process.env.BCRYPT_ROUNDS as string, 10),
     DATABASE_URL: process.env.DATABASE_URL as string,
     JWT_PASS: process.env.JWT_PASS as string,
--- src/server.ts
+++ src/server.ts
@@ -4,11 +4,9 @@ import helmet from "helmet";
 import routesV1 from "./routes/v1/v1.routes";
 import env from "./env";
 
-
 if (!env.API_PORT) {
     console.log("no port value especified, running on port 3000.");
 }
-
 const PORT = env.API_PORT || 3000;
 
 const app = express();
